ef3d75279a63459a33e26a93223ff2de446530aa
Soo-Beom <soobeomkim77@gmail.com>
2021-05-17 11:09:39 -0700

    finished code-along

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,5 @@
+let state = {count: 0}
+
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -7,7 +9,25 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function render(){
+  document.body.textContent = state.count
+}
 
-changeState(state, action)
+function dispatch(action){
+  state = changeState(state, action)
+  render();
+}
+
+render();
+
+// dispatch({type: 'INCREASE_COUNT'})
+// dispatch({type: 'INCREASE_COUNT'})
+// dispatch({type: 'INCREASE_COUNT'})
+
+// let state = {count: 0}
+// let action = {type: 'INCREASE_COUNT'}
+
+// changeState(state, action)
+
+
+
